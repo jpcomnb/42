fd7a6391cc3a6194dac13791eb4db2b0ce3402c9
jpcomnb <jp.okai@hotmail.com>
2025-04-07 22:40:08 +0100

    Delete exercises directory

--- exercises/shell00/ex06/git_ignore.sh
+++
@@ -1,2 +0,0 @@
-#ls shows files -files shows files in the git repository -l uses the long list format -d shows hidden files 
-git ls-files -others --ignored --exclude-standard
--- exercises/shell01/ex04/MAC.sh
+++
@@ -1,7 +0,0 @@
-ifconfig | grep ether | awk '{print $2}'
-#ifconfig pulls up network interface paramaters
-# | uses the output on the left for the as input for the command on the right
-# grep searches for lines containing ether
-# awk edits text and uses whitespace as the field sperator
-# {print $} tells awk to print the second field which since the MAC address
-#is separated by : and not whitespace is the entire MAC address.
