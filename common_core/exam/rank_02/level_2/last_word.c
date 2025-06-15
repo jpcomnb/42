2b03fca3e983742459b6798dba61b664e085a669
Joao Pedro Clemente Okai <jopedro4@c1r4s3.42porto.com>
2025-06-15 22:28:53 +0100

    update

--- common_core/exam/rank_02/level_2/last_word.c
+++ common_core/exam/rank_02/level_2/last_word.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   last_word.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jopedro4 <jopedro4@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/05 19:05:59 by jopedro4          #+#    #+#             */
-/*   Updated: 2025/06/15 19:57:43 by jopedro4         ###   ########.fr       */
+/*   Created: 2025/06/13 18:15:12 by jopedro4          #+#    #+#             */
+/*   Updated: 2025/06/13 18:17:12 by jopedro4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include <unistd.h>
 
-# include "libft.h"
-
-typedef struct	list_s
+int	main(int argc, char **argv)
 {
-	int		data;
-	list_s	*next;
-	list_s	*prev;
-}	list_t;
 
-void	push_swap(char	*string);
-#endif
+}
---
+++ common_core/exercises/push_swap/push_swap_commands.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_commands.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jopedro4 <jopedro4@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/15 19:33:00 by jopedro4          #+#    #+#             */
+/*   Updated: 2025/06/15 22:28:05 by jopedro4         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+list_t	*swap_a(list_t **lst_a)
+{
+	list_t	*temp;
+	int		temp_nbr;
+	int		temp_nbr2;
+
+	if (!lst_a || !(lst_a->next))
+		return ;
+	temp = lst_a;
+	temp_nbr = lst_a-> data;
+	temp = temp->next;
+	temp_nbr2 = temp->data;
+	temp->data = temp_nbr;
+	temp = temp->prev;
+	temp->data = temp_nbr2;
+	return(temp);
+}
+
+
+list_t	*swap_b(list_t **lst_b)
+{
+	list_t	*temp;
+	int		temp_nbr;
+	int		temp_nbr2;
+
+	if (!lst_a || !(lst_a->next))
+		return ;
+	temp = lst_a;
+	temp_nbr = lst_b-> data;
+	temp = temp->next;
+	temp_nbr2 = temp->data;
+	temp->data = temp_nbr;
+	temp = temp->prev;
+	temp->data = temp_nbr2;
+	return(temp);
+}
+
+list_t	*push_a(list_t *lst_a, list_t *lst_b)
+{
+	
+}
