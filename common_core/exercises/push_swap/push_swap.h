7df9d8c0fb04d21fc1a7c71f487a12762efc1536
Joao Pedro Clemente Okai <jopedro4@c1r16s9.42porto.com>
2025-07-13 23:13:41 +0100

    a

--- common_core/exercises/push_swap/push_swap.h
+++ common_core/exercises/push_swap/push_swap.h
@@ -6,19 +6,19 @@
 /*   By: jopedro4 <jopedro4@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 19:05:59 by jopedro4          #+#    #+#             */
-/*   Updated: 2025/06/15 19:57:43 by jopedro4         ###   ########.fr       */
+/*   Updated: 2025/07/05 20:45:29 by jopedro4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
 # include "libft.h"
 
 typedef struct	list_s
 {
-	int		stack_a;
-	int		stack_b
+	int		stack;
+	int		cost;
+	int		final_position;
 	list_s	*next;
 	list_s	*prev;
 }	list_t;
--- common_core/exercises/push_swap/push_swap_commands.c
+++ common_core/exercises/push_swap/push_swap_commands.c
@@ -6,32 +6,32 @@
 /*   By: jopedro4 <jopedro4@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 19:33:00 by jopedro4          #+#    #+#             */
-/*   Updated: 2025/06/15 22:28:05 by jopedro4         ###   ########.fr       */
+/*   Updated: 2025/07/10 18:22:34 by jopedro4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-list_t	*lst_swap(list_t **lst_a)
+list_t
+
+list_t	*lst_swap(list_t **lst)
 {
 	list_t	*temp;
 	int		temp_nbr;
 	int		temp_nbr2;
 
-	if (!lst_a || !(lst_a->next))
+	if (!lst || !(lst->next))
 		return ;
-	temp = lst_a;
-	temp_nbr = lst_a-> data;
-	temp = temp->next;
-	temp_nbr2 = temp->data;
-	temp->data = temp_nbr;
-	temp = temp->prev;
-	temp->data = temp_nbr2;
-	return(temp);
+	lst->prev=p
 }
 
-
-list_t	*push_a(list_t *lst_a, list_t *lst_b)
+list_t	*push(list_t *lst_1, list_t *lst_2)
 {
+	list_t *temp;
+	int		temp_nbr;
+	int		temp_nbr2
+
+	if (!lst_2)
+		return ;
 	
 }
